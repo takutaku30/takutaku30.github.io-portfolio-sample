4e80c816ff00088cce7acf4e9f209d81582610ba
takutaku30 <takutaku0730engineer@gmail.com>
2025-08-14 21:17:53 +0900

    remove folder complete

--- complete/js/index.js
+++
@@ -1,6 +0,0 @@
-// ハンバーガーボタンとドロワー
-$("#js-button-drawer").on("click", function () {
-  $(this).toggleClass("is-checked");
-  $("#js-drawer").slideToggle();
-  $("body").toggleClass("is-fixed");
-});
